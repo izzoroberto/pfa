2dbdf0af269b052e40322f6075c9b222f31c5b32
INT\roberto <mylerch@hotmail.it>
2018-04-17 14:16:04 +0200

    add example shared state in xunit

---
+++ DemoNeverCommit/DemoNeverCommitXunitTest/Integration/MyFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoNeverCommitXunitTest.Integration
+{
+    public class MyFixture : IDisposable
+    {
+        //public SqlConnection Db { get; private set; }
+        public MyFixture()
+        {
+            // Setup here
+            //Db = new SqlConnection("MyConnectionString");
+        }
+
+        public void Dispose()
+        {
+            // Cleanup here
+        }
+    }
+}
---
+++ DemoNeverCommit/DemoNeverCommitXunitTest/Integration/MyTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DemoNeverCommitXunitTest.Integration
+{
+    //Class where you want to use shared class instance
+    //XUnit executes tests in parallel by default.Having shared state/values between tests can lead to strange behaviour
+    public class MyTests : IClassFixture<MyFixture>
+    {
+        private readonly MyFixture _fixture;
+
+        public MyTests(MyFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void Test()
+        {
+            // Do something with _fixture
+        }
+    }
+}
